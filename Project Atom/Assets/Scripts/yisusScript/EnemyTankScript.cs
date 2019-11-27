4bc4d4a081c06b3f73f1ea7e2a3cfbe21dab4135
DavidMontoya1998 <disus15011998@gmail.com>
2019-11-26 22:27:18 -0600

    Ataque y muerte enemigo tanque

--- Project Atom/Assets/Scripts/yisusScript/EnemyTankScript.cs
+++ Project Atom/Assets/Scripts/yisusScript/EnemyTankScript.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class EnemyTankScript : Enemy
+public class EnemyTankScript : Enemy,IDamageable
 {
+    public float damage;
     private Transform target;
     [SerializeField]
     private Vector3 dest;
@@ -13,16 +14,19 @@ public class EnemyTankScript : Enemy
     public float distance;
     public float stopDistance;
     public float followDistance;
-    private bool atacando = false;
+    private bool rageAtack = false;
     private bool empujando = false;
     public float impulseForce;
 
+
     //Variables que necesito
+    public Material RageMaterial;
     private int state = 0;
     public float maxRageTime;
     public float maxFollowTime;
     private float rageTime = 0;
     private float followTime = 0;
+    public LayerMask WhatIsPlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,17 +38,38 @@ public class EnemyTankScript : Enemy
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Move();
+        if(life <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            Move();
+            Atack();
+        }
+        
     }
 
     public override void Atack()
     {
-        atacando = true;
-        transform.LookAt(target);
-        StartCoroutine(ImpulseAtak());
-        
+        Collider[] atakZone = Physics.OverlapSphere(transform.position, 0.6f, WhatIsPlayer);
+        if (atakZone.Length > 0)
+        {
+            for (int i = 0; i < atakZone.Length; i++)
+            {
+                if (atakZone[i].gameObject.GetComponentInParent<IDamageable>() != null)
+                {
+                    Vector3 direction = atakZone[i].transform.position - transform.position;
+                    direction.y = 0;
+                    atakZone[i].gameObject.GetComponentInParent<IDamageable>().Hurt(damage);
+                    atakZone[i].gameObject.GetComponentInParent<IDamageable>().EnableKnockback(direction, 5f, 35);
+                }
+            }
+            atakZone = null;
+        }
+
     }
 
     public override void Move()
@@ -56,7 +81,7 @@ public class EnemyTankScript : Enemy
             case 0:
                 //Esperando
                 followTime -= Time.deltaTime;
-                anim.Play("RobotIdle");
+                anim.Play("IdleTankv2");
                 agent.destination = transform.position;
                 if (dist < followDistance && followTime <= 0)
                 {
@@ -88,6 +113,8 @@ public class EnemyTankScript : Enemy
                     
                 break;
             case 2:
+                rageAtack = true;
+                enemyShape.material = RageMaterial;
                 agent.acceleration = 5;
                 agent.speed = speed * 4;
                 transform.LookAt(target);
@@ -96,43 +123,59 @@ public class EnemyTankScript : Enemy
                 followTime += Time.deltaTime;
                 if(followTime >= maxFollowTime)
                 {
+                    enemyShape.material = standarMaterial;
                     stopDistance = 100;
                     followDistance = 100;
                     followTime = maxFollowTime / 2;
+                    rageAtack = false;
                     state = 0;
                 }
                 break;
         }
-        //if (dist > stopDistance && !atacando)
-        //{
-        //    gameObject.transform.LookAt(target);
-            
-        //}
-        //else
-        //{
-        //    Atack();
-        //    anim.Play("RobotIdle");
-        //    agent.destination = transform.position;
-        //}
     }
 
     public override void Die()
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(CoDie());
     }
 
-    IEnumerator ImpulseAtak()
+    IEnumerator CoDie()
     {
-        if(!empujando)
+        agent.speed = 0;
+        agent.destination = transform.position;
+        die = true;
+        anim.Play("TankDying");
+        yield return new WaitForSeconds(4f);
+        DropItem("LifeOrb");
+        gameObject.SetActive(false);
+
+        this.gameObject.transform.parent.gameObject.GetComponent<EnemiesGenerator>().EnemiesInRoom.Remove(this.gameObject);
+    }
+
+    void IDamageable.Hurt(float Damage)
+    {
+        if(!rageAtack && !die)
         {
-            empujando = true;
-            for (int i = 0; i < 8; i++)
-            {
-                rigid.AddForce((target.transform.position - transform.position) * impulseForce, ForceMode.Acceleration);
-                yield return new WaitForSeconds(0.01f);
-            }
-            atacando = false;
+            //anim.Play("TankHit");
+            stopDistance = 100;
+            followDistance = 100;
+            hurtEffect.Play();
+            life -= Damage;
         }
         
+    }
+
+    void IDamageable.EnableKnockback(Vector3 direction, float time, float force)
+    {
+        //No pasa nada No tiene KnockBack este vato
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireSphere(transform.position,0.6f);
     }
 }
